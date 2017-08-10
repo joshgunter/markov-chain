31ba6d822cfcba3965cd496dbbec7cfc1309f36b
Joshua Gunter <jmgunter@gmail.com>
2017-08-09 21:51:12 -0700

    add poems

--- fetch_data.py
+++ fetch_data.py
@@ -1,6 +1,17 @@
 import urllib2
 
-response = urllib2.urlopen('http://www.catb.org/esr/faqs/hacker-howto.html')
-html = response.read()
+a = 'https://www.poetryfoundation.org/poems/92195/matilda-gathering-flowers'
+b = 'https://www.poetryfoundation.org/poems/45113/alastor-or-the-spirit-of-solitude'
+c = 'https://www.poetryfoundation.org/poems/45123/hymn-to-intellectual-beauty'
+d = 'https://www.poetryfoundation.org/poems/45117/the-cloud-56d2247bf4112'
+e = 'https://www.poetryfoundation.org/poems/45121/hellas-chorus'
+
+poems = [a, b, c, d , e]
+
+for item in poems:
+    response = urllib2.urlopen(item)
+    html += response.read()
+
+print html
 
 response.close
